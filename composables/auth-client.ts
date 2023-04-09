b39a2ae1193f63726948f1b5ee0d9bf3b5e517c6
JohnKougioum <john50john60@gmail.com>
2023-04-09 19:11:22 +0300

    chore: add redirect_uri for prod

--- composables/auth-client.ts
+++ composables/auth-client.ts
@@ -2,10 +2,12 @@ export const loginCookie = useCookie('loggedIn')
 
 export const isLoggedIn = computed(() => !!loginCookie.value)
 
+const redirect_uri = process.dev ? 'http://localhost:8080/login/auth' : 'http://localhost:3000/login/auth'
+
 export function redirectToLogin() {
   window.location.href = `https://login.iee.ihu.gr/authorization/?client_id=${
         useRuntimeConfig().public.CLIENT_ID
-      }&response_type=code&scope=profile&redirect_uri=http://localhost:8080/login/auth`
+      }&response_type=code&scope=profile&redirect_uri=${redirect_uri}`
 }
 
 export async function logout() {
