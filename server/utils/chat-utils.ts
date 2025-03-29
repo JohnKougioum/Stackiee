c6d321a69fb9403ed3250c9e30f52b95eb42a939
Apostolis Maloudis <73343451+tolismaloudis@users.noreply.github.com>
2025-03-29 14:25:55 +0200

    Add Latest Message Update on New Message Sent
    Added functionality to automatically refresh the latest message in a conversation as soon as a new message is sent.

--- server/utils/chat-utils.ts
+++ server/utils/chat-utils.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client'
-import { object as zobject, string as zstring } from 'zod'
+import { PrismaClient } from "@prisma/client";
+import { object as zobject, string as zstring } from "zod";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function getUserConversations(userId: string) {
   const conversations = await prisma.conversationParticipant.findMany({
@@ -26,21 +26,24 @@ export async function getUserConversations(userId: string) {
         },
       },
     },
-  })
-  return conversations
+  });
+  return conversations;
 }
 
 const createMessagePayloadSchema = zobject({
   conversationId: zstring().min(1),
   body: zstring().min(1),
-})
+});
 
-export async function addMessageDB(userId = '', conversationId = '', body = '') {
+export async function addMessageDB(
+  userId = "",
+  conversationId = "",
+  body = ""
+) {
   try {
-    createMessagePayloadSchema.parse({ conversationId, body })
-  }
-  catch (error) {
-    throw new Error('Invalid Payload')
+    createMessagePayloadSchema.parse({ conversationId, body });
+  } catch (error) {
+    throw new Error("Invalid Payload");
   }
 
   const conversation = await prisma.conversation.findFirst({
@@ -50,10 +53,13 @@ export async function addMessageDB(userId = '', conversationId = '', body = '') 
     include: {
       participants: true,
     },
-  })
+  });
 
-  if (!conversation || !conversation?.participants.some(p => p.userId === userId))
-    throw new Error('Participant doesn\'t exist in this conversation')
+  if (
+    !conversation ||
+    !conversation?.participants.some((p) => p.userId === userId)
+  )
+    throw new Error("Participant doesn't exist in this conversation");
 
   try {
     const newMessage = await prisma.message.create({
@@ -70,11 +76,19 @@ export async function addMessageDB(userId = '', conversationId = '', body = '') 
           },
         },
       },
-    })
+    });
 
-    return newMessage
-  }
-  catch (error) {
-    throw createError('Error sending message')
+    await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        latestMessage: newMessage.body,
+      },
+    });
+
+    return newMessage;
+  } catch (error) {
+    throw createError("Error sending message");
   }
 }
