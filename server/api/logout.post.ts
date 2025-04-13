31a8397a54d4fd2d450a4fdeea5cc44f908e5f27
JohnKougioum <jkougioumtzidis@gmail.com>
2025-04-13 17:46:39 +0300

    feat: delete read notifications on logout

--- server/api/logout.post.ts
+++ server/api/logout.post.ts
@@ -1,4 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export default defineEventHandler(async (event) => {
+  const userId = event.context.id.id
+
+  await prisma.notification.deleteMany({
+    where: {
+      userId,
+      hasSeen: true,
+    },
+  })
+
   deleteCookie(event, 'loggedIn')
   deleteCookie(event, 'token')
   return {
