565d52ba7081485693ae728ec1e35de258959d5f
JohnKougioum <jkougioumtzidis@gmail.com>
2025-05-29 21:08:55 +0300

    feat: footer scroll up button

--- components/nav/NavFooter.vue
+++ components/nav/NavFooter.vue
@@ -3,10 +3,9 @@
 </script>
 
 <template>
-  <div class="p-4">
-    Footer
-    <span class="cursor-pointer" @click="$scrollToTop">
-      Top
-    </span>
+  <div class="p-4 pl-8">
+    <button class="btn-icon" @click="$scrollToTop">
+      <Icon name="majesticons:arrow-up" size="1.5em" />
+    </button>
   </div>
 </template>
